ef342ddf0b3997a960f7af9908e4aa9c8878f02e
Prabin Rai <chokhang777@gmail.com>
2021-01-31 11:02:48 -0500

    updates

--- learn-terrraform-aws/dev/main.tf
+++ learn-terrraform-aws/dev/main.tf
@@ -13,18 +13,18 @@ provider "aws" {
 
 }
 
-module "vpc" {
+module "my_vpc" {
   source      = "../modules/vpc"
   vpc_cidr    = "192.168.0.0/16"
   tenancy     = "default"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.my_vpc.output_vpc_id
   subnet_cidr = "192.168.1.0/24"
 }
 
 module "ec2" {
   source         = "../modules/ec2"
-  instance_count = "1"
+  instance_count = 1
   ami_id         = "ami-0a0ad6b70e61be944"
   instance_type  = "t2.micro"
-  subnet_id      = module.vpc.subnet_id
+  subnet_id      = module.my_vpc.output_subnet_id
 }
